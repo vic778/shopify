94ae9b7a0544194cd66b0ff4e1ebede47e16fae9
Victoir Barh <77842237+vic778@users.noreply.github.com>
2022-08-13 17:59:50 +0300

    Webhooks (#5)
    * Convert price
    
    * Create webhooks credentials
    
    * Route for webhooks
    
    * Update the sale_acount colum
    
    * Update the sale_acount colum

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # resources :checkouts, only: [:create]
   # post "checkout/create", to: "checkout#create"
   post "checkout/create", to: "checkout#create"
+  resources :webhooks, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
