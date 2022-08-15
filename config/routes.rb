a5627a9c06fa4955600187182e715655b3a4b805
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-15 17:48:37 +0300

    Create cancel route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # resources :checkouts, only: [:create]
   # post "checkout/create", to: "checkout#create"
   post "checkout/create", to: "checkout#create"
+  get "success", to: "checkout#success"
+  get "cancel", to: "checkout#cancel"
   resources :webhooks, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
