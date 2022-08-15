645e69039d302c4171b4c175111e7de4ac9278e2
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-15 11:13:47 +0300

    Stripe uprate product coloum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_071511) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_075532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_071511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sales_count", default: 0, null: false
+    t.string "stripe_product_id"
   end
 
   create_table "users", force: :cascade do |t|
