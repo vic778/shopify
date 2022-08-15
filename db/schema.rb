e2ea735d9d8be81ce451d70c1d8d6bf31185c2ac
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-15 11:27:34 +0300

    Add stripe price colomn to item table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_075532) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_081737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_075532) do
     t.datetime "updated_at", null: false
     t.integer "sales_count", default: 0, null: false
     t.string "stripe_product_id"
+    t.string "stripe_price_id"
   end
 
   create_table "users", force: :cascade do |t|
