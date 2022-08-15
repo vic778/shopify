98e295748c7503a06180f784f3fa4fe7e2b09d9f
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-15 19:16:39 +0300

    Colect all items from the jbuler format

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,15 +1,11 @@
 class CheckoutController < ApplicationController
 
     def create
-        @item = Item.find(params[:id])
+    
         @session = Stripe::Checkout::Session.create({
             customer: current_user.stripe_customer_id,
             payment_method_types: ['card'],
-            line_items: [{
-
-                price: @item.stripe_price_id,
-              quantity: 1
-            }],
+            line_items: @cart.collect { |item| item.to_builder.attributes! },
             mode: 'payment',
             # discounts: [{
             #   coupon: '{{COUPON_ID}}',
@@ -26,8 +22,8 @@ class CheckoutController < ApplicationController
     end
 
   def success
-    session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ['list_line_items']})
-    session_with_expand.list_line_items.data.each do |line_item|
+    session_with_expand = Stripe::Checkout::Session.retrieve({id: params[:session_id], expand: ['line_items']})
+    session_with_expand.line_items.data.each do |line_item|
       item = Item.find_by(stripe_product_id: line_item.price.product)
       item.increment!(:sales_count)
     end
