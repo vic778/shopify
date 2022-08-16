c29395c6f42c3eb1a10c8ece66d2a9295208ef62
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-16 19:18:46 +0300

    Config rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,30 +1,29 @@
 class Item < ApplicationRecord
-    validates :name, presence: true
-    validates :price, presence: true
-    monetize :price, as: "price_cents"
+  validates :name, presence: true
+  validates :price, presence: true
+  monetize :price, as: "price_cents"
 
-    def to_s
-        name
+  def to_s
+    name
+  end
+
+  after_create do
+    product = Stripe::Product.create(name: name)
+    price = Stripe::Price.create(product: product.id, unit_amount: self.price, currency: currency)
+    update(stripe_product_id: product.id, stripe_price_id: price.id)
+  end
+
+  after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
+  def create_and_assign_new_stripe_price
+    # product = Stripe::Product.retrieve(stripe_product_id)
+    price = Stripe::Price.create(product: stripe_product_id, unit_amount: self.price, currency: currency)
+    update(stripe_price_id: price.id)
+  end
+
+  def to_builder
+    Jbuilder.new do |product|
+      product.price stripe_price_id
+      product.quantity 1
     end
-
-    after_create do
-        product = Stripe::Product.create(name: name)
-        price = Stripe::Price.create(product: product.id, unit_amount: self.price, currency: self.currency)
-        update(stripe_product_id: product.id, stripe_price_id: price.id)
-    end
-
-    after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
-    def create_and_assign_new_stripe_price
-        # product = Stripe::Product.retrieve(stripe_product_id)
-        price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: self.currency)
-        update(stripe_price_id: price.id)
-    end
-
-
-    def to_builder
-        Jbuilder.new do |product|
-          product.price stripe_price_id
-          product.quantity 1
-        end
-    end
+  end
 end
