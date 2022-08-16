718cdd35e2ce474ec69013a54c7ccad789f3b127
Victoir Barh <77842237+vic778@users.noreply.github.com>
2022-08-16 19:20:09 +0300

    Code promo (#21)
    * Create code promo
    
    * Permit currency
    
    * Update stripe currency
    
    * Hiden the currency cololumn
    
    * Migrate currency col
    
    * Add curency to items
    
    * Add moeny-rails
    
    * Add moeny-rails
    
    * Permit money_rails
    
    * Monetize the price colomn as price_cents
    
    * Change the price to price_cents
    
    * Set the default currency to usd
    
    * Display the price_cents insteaof of price
    
    * dd rubocop
    
    * dd rubocop
    
    * Fix linters error
    
    * Fix linters error
    
    * Fix linters error
    
    * Config rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_081737) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_151739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_081737) do
     t.integer "sales_count", default: 0, null: false
     t.string "stripe_product_id"
     t.string "stripe_price_id"
+    t.string "currency"
   end
 
   create_table "users", force: :cascade do |t|
