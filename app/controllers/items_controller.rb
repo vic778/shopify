008fa91f2c34134a05f49f72222c2e2e5f8121be
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-16 19:02:39 +0300

    Permit money_rails

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -77,6 +77,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :price, :currency)
+      params.require(:item).permit(:name, :price, :price_cents, :currency)
     end
 end
