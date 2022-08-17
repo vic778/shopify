9be1b43155a686c72a0ffa6b05fbc3a13a7ee28a
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-17 17:21:49 +0300

    Permit pictures and make it as an array

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -76,6 +76,6 @@ class ItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def item_params
-    params.require(:item).permit(:name, :price, :price_cents, :currency)
+    params.require(:item).permit(:name, :price, :price_cents, :currency, :body, :image, pictures: [])
   end
 end
