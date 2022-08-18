2dd38897ab5c636b99c257393756814ce712c983
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-18 11:25:53 +0300

    Resize image

--- app/models/item.rb
+++ app/models/item.rb
@@ -32,6 +32,18 @@ class Item < ApplicationRecord
   end
 
   def image_as_thumbnail
+    return unless image.content_type.in?(%w[image/jpeg image/png])
+
     image.variant(resize_to_limit: [300, 300]).processed
+  end
+
+  def pictures_as_thumbnails
+    pictures.map do |picture|
+      picture.variant(resize_to_limit: [150, 150]).processed
+    end
+  end
+
+  def pictures_as_thumbnail(pic)
+    pic.variant(resize_to_limit: [150, 150]).processed
   end
 end
